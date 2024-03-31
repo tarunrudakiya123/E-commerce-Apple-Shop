5c1fc61e6b83e54b14342f5fa5a03937c7320740
Your Name <you@example.com>
2024-03-31 14:28:17 +0530

    add imeges

--- src/Component/Rating.js
+++ src/Component/Rating.js
@@ -3,58 +3,8 @@ export default function Rating(props) {
     return (
         <>
             {
-                rating < 1 ? (
-                    <>
-                        <i className="fa-solid fa-star-half-stroke"></i>
-                        <i className="fa-regular fa-star"></i>
-                        <i className="fa-regular fa-star"></i>
-                        <i className="fa-regular fa-star"></i>
-                        <i className="fa-regular fa-star"></i>
-                    </>
-                ) : (
-                    rating === 1 ? (
-                        <>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-                        </>
-                    ) : rating < 2 ? (
-                        <>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-solid fa-star-half-stroke"></i>
-                            <i className="fa-regular fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-
-                        </>
-                    ) : rating === 2 ? (
-                        <>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-                        </>
-                    ) : rating < 3 ? (
-                        <>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-solid fa-star-half-stroke"></i>
-                            <i className="fa-regular fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-                        </>
-                    ) : rating === 3 ? (
-                        <>
-
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-                            <i className="fa-regular fa-star"></i>
-                        </>
-                    ) : rating < 4 ? (
+                (
+                   rating < 4 ? (
                         <>
                             <i className="fa-solid fa-star"></i>
                             <i className="fa-solid fa-star"></i>
